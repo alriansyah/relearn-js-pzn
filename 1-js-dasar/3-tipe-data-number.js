d6dd44a8281a7f1ceb04331347b33a27d41f61c3
alriansyah <alriansyah1218@gmail.com>
2024-08-29 10:24:55 +0700

    feat: learn js tipe data number

---
+++ 1-js-dasar/3-tipe-data-number.js
@@ -0,0 +1,29 @@
+const number1 = 10;
+const number2 = 20.5;
+
+console.log(typeof number1);
+console.log(typeof number2);
+console.log("--------------------------");
+
+/*
+JavaScript mendukung number notation berikut :
+- Defaultnya berbasis decimal / 10
+- Hexadecimal = 0x
+- Binary = 0b
+- Octal = 0o
+*/
+
+const decimal = 100;
+const hexadecimal = 0x100;
+const binary = 0b100;
+const octal = 0o100;
+
+console.log(decimal);
+console.log(hexadecimal);
+console.log(binary);
+console.log(octal);
+console.log("--------------------------");
+console.log(typeof decimal);
+console.log(typeof hexadecimal);
+console.log(typeof binary);
+console.log(typeof octal);
