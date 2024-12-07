98cf6da929203de2eda2fb40134a6f8201d7d81e
alriansyah <alriansyah1218@gmail.com>
2024-12-07 08:44:25 +0700

    feat: learn js operator perbandingan

---
+++ 1-js-dasar/8-operator-perbandingan.js
@@ -0,0 +1,23 @@
+/*
+    Operator Perbandingan :
+    < (Kurang dari)
+    > (Lebih dari)
+    <= (Kurang dari sama dengan)
+    >= (Lebih dari sama dengan)
+    == (Sama dengan)
+    === (Sama dengan dan sama tipe data)
+    != (Tidak sama dengan)
+    !== (Tidak sama dengan dan tidak sama tipe data)
+*/
+
+const number1 = 2;
+const number2 = 3;
+
+console.log(number1 < number2);
+console.log(number1 > number2);
+console.log(number1 <= number2);
+console.log(number1 >= number2);
+console.log(number1 == number2);
+console.log(number1 === number2);
+console.log(number1 != number2);    
+console.log(number1 !== number2);
