58dfabd0a1a383c7178e08aa942a6c3ec043fb75
alriansyah <alriansyah1218@gmail.com>
2024-12-08 19:35:20 +0700

    feat: learn js operator logika

---
+++ 1-js-dasar/9-operator-logika.js
@@ -0,0 +1,37 @@
+/*
+    Operator Logika :
+    && (Logical AND)
+    || (Logical OR)
+    ! (Logical NOT)
+*/
+
+// AND
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+console.log("----------");
+
+
+// OR
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+console.log("----------");
+
+// NOT
+console.log(!true);
+console.log(!false);
+console.log("----------");
+
+/*
+    Operator Unary :
+    ! (Unary Not)
+*/
+console.log(!true);
+console.log(!false);
+
+
+
+
