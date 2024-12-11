e0150babf7589ad01cfc2456dfaa699f86a213cc
alriansyah <alriansyah1218@gmail.com>
2024-12-12 03:05:32 +0700

    feat: lean js null

---
+++ 1-js-dasar/18-null.js
@@ -0,0 +1,12 @@
+// Null : representasi data kosong
+let und; // undefined
+let nama = null; // null
+
+if (nama === null) {
+    console.log("Null");
+} else if (nama === undefined) {
+    console.log("Undefined");
+} else {
+    console.log(nama);
+}
+console.log("-----------------------");
