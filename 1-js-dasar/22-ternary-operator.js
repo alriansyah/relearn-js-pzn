70148150c48208a38fd1cf4b1614b6f04c3c26c8
alriansyah <alriansyah1218@gmail.com>
2024-12-12 03:19:17 +0700

    feat: learn js ternary operator

---
+++ 1-js-dasar/22-ternary-operator.js
@@ -0,0 +1,9 @@
+// Ternary Operator : operasi yang lebih sederhana dari if else /  if statement
+let nama = "Al Riansyah";
+nama === undefined ? console.log("Undefined") : console.log(nama);
+
+const result = nama === undefined ? "Undefined" : nama;
+console.log(result);
+
+const ucapan = nama === undefined ? "Undefined" : "Hello Kamu";
+console.log(ucapan);
