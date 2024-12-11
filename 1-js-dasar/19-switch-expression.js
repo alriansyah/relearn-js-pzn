eb7d72a3094c4031241773f6f62a8cbc74448a1b
alriansyah <alriansyah1218@gmail.com>
2024-12-12 03:08:59 +0700

    feat: learn js switch expression

---
+++ 1-js-dasar/19-switch-expression.js
@@ -0,0 +1,18 @@
+// Switch merupakan salah satu statement di javascript yang mirip sama dengan if else
+// namun lebih sederhana pembuatannya
+// Switch case hanya untuk perbandingan ==
+const nilai = "C";
+
+switch (nilai) {
+    case "A":
+        console.log("Nilai sangat baik");
+        break;
+    case "B":
+        console.log("Nilai baik");
+        break;
+    case "C":
+        console.log("Nilai cukup");
+        break;
+    default:
+        console.log("Maaf anda tidak lulus");
+}
