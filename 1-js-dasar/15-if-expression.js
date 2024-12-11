71eb36d2f843fa9ebdcfb01cbe0c5f62e914284d
alriansyah <alriansyah1218@gmail.com>
2024-12-12 02:57:24 +0700

    feat: learn js if expression

---
+++ 1-js-dasar/15-if-expression.js
@@ -0,0 +1,19 @@
+const nilai = 90;
+
+if (nilai > 80) {
+    console.log("Selamat anda lulus");
+} else {
+    console.log("Maaf anda tidak lulus");
+
+}
+
+
+if (nilai > 80) {
+    console.log("Nilai A");
+} else if (nilai > 60) {
+    console.log("Nilai B");
+} else if (nilai > 50) {
+    console.log("Nilai C");
+} else {
+    console.log("Maaf anda tidak lulus");
+}
