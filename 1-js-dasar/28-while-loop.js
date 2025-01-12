f667f3a553f3423e418f359a58760c3ddc0e3734
alriansyah <alriansyah1218@gmail.com>
2025-01-12 16:48:13 +0700

    feat: learn js while loop

---
+++ 1-js-dasar/28-while-loop.js
@@ -0,0 +1,7 @@
+// while loop lebih sederhana dari for loop
+// Pengecekan kondisi di while loop dilakukan di awal sebelum perulangan dilakukan
+let count = 1;
+while (count <= 10) {
+    console.log(count);
+    count++;
+}
