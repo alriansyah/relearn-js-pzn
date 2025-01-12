98ff09eaf72f93e863ecf74c0e91a78395b9bf77
alriansyah <alriansyah1218@gmail.com>
2025-01-12 16:46:06 +0700

    feat: learn js for loop

---
+++ 1-js-dasar/27-for-loop.js
@@ -0,0 +1,9 @@
+/* 
+for(init statement; condition; update/post statement){
+    // init hanya di jalankan sekali di awal
+}
+*/
+const nilai = 100;
+for (let i = 1; i <= nilai; i++) {
+    console.log(i);
+}
