ffdedf6cf270856e613dec37818cbb58065cb6be
alriansyah <alriansyah1218@gmail.com>
2025-01-19 16:07:44 +0700

    feat: learn js function parameter

---
+++ 1-js-dasar/36-function-parameter.js
@@ -0,0 +1,9 @@
+function perkenalan(params) {
+  console.log(`Perkenalkan nama saya : ${params}`);
+}
+perkenalan("Al Riansyah");
+
+function perhitungan(a, b) {
+  console.log(a + b);
+}
+perhitungan(10, 20);
