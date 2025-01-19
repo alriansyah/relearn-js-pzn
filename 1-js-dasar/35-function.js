a357ed2abf4bbdc3c059e18b719c20134f924aa9
alriansyah <alriansyah1218@gmail.com>
2025-01-19 15:42:23 +0700

    feat: learn js function

---
+++ 1-js-dasar/35-function.js
@@ -0,0 +1,11 @@
+function perkenalan() {
+  console.log("Perkenalkan nama saya : Al Riansyah");
+}
+perkenalan();
+
+// Function parameter :
+function functionParam(parameter) {
+  console.log(`Perkenalkan nama saya : ${parameter}`);
+}
+
+functionParam("Parameter");
