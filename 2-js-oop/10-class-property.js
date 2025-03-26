5fe19c5f6641f4802671239c45a44b6e8d90eb46
alriansyah <alriansyah1218@gmail.com>
2025-03-26 22:00:07 +0700

    feat: learn js opp property di class

---
+++ 2-js-oop/10-class-property.js
@@ -0,0 +1,11 @@
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+const eko = new Person("Eko");
+console.info(eko);
+console.info(eko.name);
+
+const budi = new Person("Budi");
