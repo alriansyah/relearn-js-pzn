13c97911450cd567a6f40430facb4f5014670fa7
alriansyah <alriansyah1218@gmail.com>
2025-03-26 21:55:04 +0700

    feat: learn js opp constructor di class

---
+++ 2-js-oop/9-class-constructor.js
@@ -0,0 +1,11 @@
+class Person {
+  constructor(name) {
+    // kode apapun
+    console.info(`Membuat Person ${name}`);
+  }
+}
+
+const eko = new Person("Eko");
+console.info(eko);
+
+const budi = new Person("Budi");
