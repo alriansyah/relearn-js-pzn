d67e566714b65ab2ccd7d8e45ea000fa2b659249
alriansyah <alriansyah1218@gmail.com>
2025-03-26 21:41:31 +0700

    feat: learn js opp class

---
+++ 2-js-oop/8-class.js
@@ -0,0 +1,11 @@
+/*
+    Membuat class :
+    - Sejak EcmaScript versi 6, diperkenalkan kata kunci baru, yaitu class, ini merupakan kata kunci yang digunakan untuk membuat class di JavaScript
+    - Dengan kata kunci class, kita tidak perlu lagi menggunakan constructor function untuk membuat class
+    - Class juga berbasis prototype seperti constructor function
+*/
+
+class Person {}
+
+const eko = new Person();
+console.info(eko);
