b4b89a60b9ac9a347e54dc0fff06e83703b55ccc
alriansyah <alriansyah1218@gmail.com>
2025-05-13 00:35:16 +0700

    feat: learn js modules export default

---
+++ 4-js-modules/scripts/default-and-named.js
@@ -0,0 +1,8 @@
+export const title = "Programmer Zaman Now";
+
+const content = "Belajar JavaScript Module";
+
+const author = "Eko Kurniawan Khannedy";
+
+export {content};
+export default author; // dalam modul, kita tidak boleh export default lebih dari satu, namun kita bisa menggabungkan export default dengan export named
---
+++ 4-js-modules/scripts/default-variable.js
@@ -0,0 +1,4 @@
+
+const name = "Eko";
+
+export default name;
